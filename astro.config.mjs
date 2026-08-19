b1e96c9b7dbb8b2c23592a2a6f37b8a759e2f067
szerlingjan <szerlingjan@gmail.com>
2026-08-19 12:40:22 +0200

    Update base path in Astro configuration to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://janphoto.eu",
-  base: "/portfolio/",
+  base: "/",
   devToolbar: {
     enabled: false,
   },
